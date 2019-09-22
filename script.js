6469f6f269353d0012d44e2eac35644dc1e4b2e6
Sayyed Jamal Ghasemi <jamal13647850@gmail.com>
2019-09-22 22:37:15 +0330

    Set scripts

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+window.addEventListener('DOMContentLoaded', () => {
+    let count = 0;
+    slider();
 
+    function slider() {
+        let image = document.getElementsByClassName('image');
+        for (let o of image) {
+            o.style.display = "none";
+        }
+        count++;
+        if (count > image.length) {
+            count = 1;
+        }
+        image[count - 1].style.display = "block";
+        setTimeout(slider, 2000);
+    }
+});
